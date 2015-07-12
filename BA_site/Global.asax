354b03db6040cd3a2258755ee9c2ef7ee3319249
JhamesBrown <jamestron@gmail.com>
2015-07-12 14:00:49 +1200

    InitialCommit

---
+++ BA_site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BA_site.MvcApplication" Language="C#" %>
---
+++ BA_site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BA_site.Startup))]
+namespace BA_site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
