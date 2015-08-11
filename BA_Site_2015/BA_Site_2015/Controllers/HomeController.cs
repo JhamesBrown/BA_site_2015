14b53f91899a30129cb3758c6106c13bb59dc1fc
JhamesBrown <jamestron@gmail.com>
2015-08-11 14:37:25 +1200

    Published site

--- BA_Site_2015/BA_Site_2015/Controllers/HomeController.cs
+++ BA_Site_2015/BA_Site_2015/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace BA_Site_2015.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Bad Artifacts";
 
             return View();
         }
